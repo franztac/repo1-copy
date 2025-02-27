7ee923c8f113598a362c6d9376f3ee417dfe57ad
franztac <fratacc91@gmail.com>
2025-02-27 13:13:09 +0100

    added burger and pizza

--- hungry.py
+++ hungry.py
@@ -1,6 +1,8 @@
 hungry = input("Are you hungry ?")
 if hungry == "yes" and hungry == "Y":
     print("eat samosa")
+    print("eat pizza")
+    print("eat burger")
 
 else:
     thirsty = input("are you thirsty?")

3601dad7fdde277742070f43341a93fd9a65abcd
franztac <fratacc91@gmail.com>
2025-02-27 12:14:51 +0100

    Initial commit

---
+++ hungry.py
@@ -0,0 +1,11 @@
+hungry = input("Are you hungry ?")
+if hungry == "yes":
+    print("eat samosa")
+    print("eat pizza")
+    print("eat burger")
+    print("eat fries")
+else:
+    thirsty = input("Are you thirsty?")
+    if thirsty == "yes":
+        print("drink water")
+        print("drink soda")

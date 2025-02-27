79ed1e73f701b5719800bb44dd683f11f0670d22
franztac <fratacc91@gmail.com>
2025-02-27 13:14:13 +0100

    pippo1

--- hungry.py
+++ hungry.py
@@ -1,5 +1,5 @@
 hungry = input("Are you hungry ?")
-if hungry == "yes" and hungry == "Y":
+if hungry == "yes" and hungry == "Y" and hungry == "pippo1":
     print("eat samosa")
     print("eat pizza")
     print("eat burger")
